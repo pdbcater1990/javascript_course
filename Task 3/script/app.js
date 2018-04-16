fc79a64b977f4479b0c9189c5cbdfd84acd03e73
Paul Cater <hello@paul-cater.co.uk>
2018-04-16 15:10:41 +0100

    change 1

--- Task 3/script/app.js
+++ Task 3/script/app.js
@@ -8,3 +8,9 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+let scores = [0,0];
+let roundScore = 0;
+let activePlayer = 0;
+
+let dice = Math.floor(Math.random * 6) + 1;
